f16ec8db35812d2be1db9b13d8ec3e33963fb516
Akshay <aktwo@princeton.edu>
2014-03-09 20:53:00 -0400

    Cleanup messages and error redirects

--- app/controllers/countdown_users/confirmations_controller.rb
+++ app/controllers/countdown_users/confirmations_controller.rb
@@ -7,14 +7,17 @@ class CountdownUsers::ConfirmationsController < Devise::ConfirmationsController
 
     unless countdown_user.nil?
       if countdown_user.confirmed?
-        set_flash_message(:notice, :confirmed)
+        set_flash_message(:notice, :already_confirmed)
+        redirect_to new_confirmation_path(resource_name)
       else
         set_flash_message(:notice, :confirmation_already_sent)
+        redirect_to new_confirmation_path(resource_name)
       end
       self.resource = countdown_user
     else
       if email_is_valid
         self.resource = CountdownUser.create(email:email)
+        set_flash_message(:notice, :send_instructions)
         yield resource if block_given?
       else
         set_flash_message(:notice, :invalid_email)
