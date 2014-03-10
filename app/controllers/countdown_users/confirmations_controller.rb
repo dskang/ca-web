9bc6966bb65c81d20d2a6efbebfdfac44ac37640
Akshay <aktwo@princeton.edu>
2014-03-10 17:58:04 -0400

    Code refactor and error checking
    - Code cleanup and rephrasing
    - Handle duplicate clicks from email
    - Add method for linking countdown_user and school

--- app/controllers/countdown_users/confirmations_controller.rb
+++ app/controllers/countdown_users/confirmations_controller.rb
@@ -4,7 +4,18 @@ class CountdownUsers::ConfirmationsController < Devise::ConfirmationsController
     email = params[resource_name][:email]
     countdown_user = CountdownUser.find_by(email:email)
 
-    unless countdown_user.nil?
+    if countdown_user.nil?
+      new_countdown_user = CountdownUser.create(email:email)
+      new_countdown_user.set_school_id!
+      if new_countdown_user.confirmation_token.nil?
+        set_flash_message(:notice, :invalid_email)
+        redirect_to new_confirmation_path(resource_name)
+      else
+        self.resource = new_countdown_user
+        set_flash_message(:notice, :send_instructions)
+        yield resource if block_given?
+      end
+    else
       if countdown_user.confirmed?
         set_flash_message(:notice, :already_confirmed)
         redirect_to new_confirmation_path(resource_name)
@@ -12,21 +23,14 @@ class CountdownUsers::ConfirmationsController < Devise::ConfirmationsController
         set_flash_message(:notice, :confirmation_already_sent)
         redirect_to new_confirmation_path(resource_name)
       end
-    else
-      new_countdown_user = CountdownUser.create(email:email, schoolid: get_school_id)
-      if new_countdown_user.confirmation_token.nil?
-        set_flash_message(:notice, :invalid_email)
-        redirect_to new_confirmation_path(resource_name)
-      else
-        set_flash_message(:notice, :send_instructions)
-        yield resource if block_given?
-      end
     end
   end
 
   def show
     countdown_user = CountdownUser.find_by(confirmation_token: params[:confirmation_token])
-    unless countdown_user.nil?
+    if countdown_user.nil?
+      set_flash_message(:notice, :already_confirmed)
+    else
       countdown_user.confirm!
       countdown_user.school.increment!(:signups)
       set_flash_message(:notice, :confirmed)
--- app/models/countdown_user.rb
+++ app/models/countdown_user.rb
@@ -4,7 +4,7 @@ class CountdownUser < ActiveRecord::Base
   belongs_to :school
 
   validates :email, presence: true, length: {maximum: 50}
-  validate  :school_is_allowed
+  validate  :school_is_allowed?
 
   def get_school_name
     pattern = /\A[\w+\-.]+@(?<school_name>\w+).edu\z/i
@@ -12,25 +12,31 @@ class CountdownUser < ActiveRecord::Base
     match.nil? ? match : match[:school_name]
   end
 
-  def school_is_allowed
+  def school_is_allowed?
     allowed_domains = [/princeton/i, /harvard/i, /yale/i, /brown/i, /upenn/i, /columbia/i, /dartmouth/i, /cornell/i]
     if get_school_name.nil?
       errors.add(:email, "Not a valid email address!")
+      return false
+    elsif allowed_domains.any? {|school| school =~ get_school_name}
+      return true
     else
-      errors.add(:email, "Not a valid .edu domain!") unless allowed_domains.any? {|school| school =~ get_school_name}
+      errors.add(:email, "Not a valid .edu domain!")
+      return false
     end
   end
 
-  def get_school_id
+  def set_school_id!
     name = get_school_name
     if name.nil?
-      return nil
+      self.update(school_id: nil)
     else
-      school = School.find_by(name:name.titleize) #FIXME
-      if school.nil?
-        return nil
+      school = School.find_by(name:name)
+      if school.nil? and self.school_is_allowed?
+        new_school = School.create(name: self.get_school_name, signups: 0)
+        new_school.save
+        self.update(school_id: new_school.id)
       else
-        return school.id
+        self.update(school_id: school.id)
       end
     end
   end
