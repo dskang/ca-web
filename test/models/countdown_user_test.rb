faa14f7a287e0697335d75ef873f4735585c174d
Akshay <aktwo@princeton.edu>
2014-03-09 22:18:48 -0400

    Added unit tests for email validation

--- test/models/countdown_user_test.rb
+++ test/models/countdown_user_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class CountdownUserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "email validation" do
+    assert_not CountdownUser.new(email:"foo@bar.com").valid?
+    assert_not CountdownUser.new(email:"test@duke.edu").valid?
+    assert_not CountdownUser.new(email:"hi").valid?
+    assert CountdownUser.new(email:"test@harvard.edu").valid?
+    assert CountdownUser.new(email:"test@princeton.edu").valid?
+    assert CountdownUser.new(email:"test@upenn.edu").valid?
+    assert CountdownUser.new(email:"test@columbia.edu").valid?
+    assert CountdownUser.new(email:"test@cornell.edu").valid?
+    assert CountdownUser.new(email:"test@dartmouth.edu").valid?
+    assert CountdownUser.new(email:"test@yale.edu").valid?
+    assert CountdownUser.new(email:"test@brown.edu").valid?
+  end
 end
