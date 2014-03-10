d33634c8d5d0052823ab20fee25a418f16f4a0b5
Akshay <aktwo@princeton.edu>
2014-03-09 20:43:36 -0400

    Added ivy .edu email validation

--- app/controllers/countdown_users/confirmations_controller.rb
+++ app/controllers/countdown_users/confirmations_controller.rb
@@ -1,44 +1,33 @@
 class CountdownUsers::ConfirmationsController < Devise::ConfirmationsController
 
   def create
-
     email = params[resource_name][:email]
     countdown_user = CountdownUser.find_by(email:email)
+    email_is_valid = CountdownUser.new(email:email).valid?
 
     unless countdown_user.nil?
       if countdown_user.confirmed?
-        self.resource = countdown_user
         set_flash_message(:notice, :confirmed)
       else
-        set_flash_message(:notice, "We've already sent you a confirmation email to #{email}, check your inbox!")
+        set_flash_message(:notice, :confirmation_already_sent)
       end
+      self.resource = countdown_user
     else
-      self.resource = CountdownUser.create(email:email)
-      yield resource if block_given?
-      if successfully_sent?(resource)
-        respond_with(resource)
-        # respond_with({}, location: after_resending_confirmation_instructions_path_for(resource_name))
+      if email_is_valid
+        self.resource = CountdownUser.create(email:email)
+        yield resource if block_given?
       else
-        respond_with(resource)
+        set_flash_message(:notice, :invalid_email)
+        redirect_to new_confirmation_path(resource_name)
       end
     end
   end
 
   def show
     self.resource = CountdownUser.find_by(confirmation_token: params[:confirmation_token])
-
     unless self.resource.nil?
       self.resource.confirm!
-      # self.resource = resource_class.confirm_by_token(params[:confirmation_token])
-      yield resource if block_given?
-
-      if resource.errors.empty?
-        set_flash_message(:notice, :confirmed) if is_flashing_format?
-        respond_with_navigational(resource){redirect_to confirmation_path(resource_name)}
-        # respond_with_navigational(resource){ redirect_to after_confirmation_path_for(resource_name, resource) }
-      else
-        respond_with_navigational(resource.errors, status: :unprocessable_entity){ render :new }
-      end
+      set_flash_message(:notice, :confirmed)
     end
   end
 end
--- app/models/countdown_user.rb
+++ app/models/countdown_user.rb
@@ -2,4 +2,7 @@ class CountdownUser < ActiveRecord::Base
 
   devise :confirmable
 
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@(brown|harvard|yale|princeton|columbia|dartmouth|cornell|upenn).edu\z/
+  validates :email, presence: true, length: {maximum: 50}, format: { with: VALID_EMAIL_REGEX}
+
 end
