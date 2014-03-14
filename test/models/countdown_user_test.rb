30ef123a4423c2a95478b31cf6f6a67f286abd9b
Dan Kang <dan@dskang.com>
2014-03-14 01:34:00 -0400

    test mismatching emails for school
    make sure to run 'RAILS_ENV=test rake db:seed' beforehand

--- test/models/countdown_user_test.rb
+++ test/models/countdown_user_test.rb
@@ -2,7 +2,18 @@ require 'test_helper'
 
 class CountdownUserTest < ActiveSupport::TestCase
   test "should not save countdown user without school" do
-    countdown_user = CountdownUser.new(email:"aktwo@princeton.edu")
-    assert_not countdown_user.save
+    countdown_user = CountdownUser.new(email: "hi@princeton.edu")
+    assert !countdown_user.save
+  end
+
+  test "should not save mismatching email for school" do
+    princeton = School.find_by(name: "princeton")
+    assert !princeton.nil?
+
+    user1 = CountdownUser.new(email: "hi@princeton.edu", school_id: princeton.id)
+    assert user1.save
+
+    user2 = CountdownUser.new(email: "hi@harvard.edu", school_id: princeton.id)
+    assert !user2.save
   end
 end
