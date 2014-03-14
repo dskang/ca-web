661cde3c8817699a5fabf535bc846b4c7d558e90
Akshay <aktwo@princeton.edu>
2014-03-14 15:47:25 -0400

    Added new images and refactored homepage code

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
+    @schools = %w(princeton harvard yale brown upenn columbia dartmouth cornell)
+    @number_of_rows = 2
   end
 
   def about
