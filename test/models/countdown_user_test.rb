6970e1a6b411ccca16354195a65a3bf561f2421a
Dan Kang <dan@dskang.com>
2014-03-14 16:39:21 -0400

    use assert_not instead of assert

--- test/models/countdown_user_test.rb
+++ test/models/countdown_user_test.rb
@@ -3,23 +3,23 @@ require 'test_helper'
 class CountdownUserTest < ActiveSupport::TestCase
   test "should not save countdown user without school" do
     countdown_user = CountdownUser.new(email: "hi@princeton.edu")
-    assert !countdown_user.save
+    assert_not countdown_user.save
   end
 
   test "should not save mismatching email for school" do
     princeton = School.find_by(name: "princeton")
-    assert !princeton.nil?
+    assert_not princeton.nil?
 
     user1 = CountdownUser.new(email: "hi@princeton.edu", school_id: princeton.id)
     assert user1.save
 
     user2 = CountdownUser.new(email: "hi@harvard.edu", school_id: princeton.id)
-    assert !user2.save
+    assert_not user2.save
 
     user3 = CountdownUser.new(email: "hi@duke.edu", school_id: princeton.id)
-    assert !user3.save
+    assert_not user3.save
 
     user4 = CountdownUser.new(email: "foo@bar.com", school_id: princeton.id)
-    assert !user4.save
+    assert_not user4.save
   end
 end
