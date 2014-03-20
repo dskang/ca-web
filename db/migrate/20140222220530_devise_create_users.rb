710380369c62d9e0f1c92fe679e0eaee7effa1c9
Akshay <aktwo@princeton.edu>
2014-03-20 18:09:16 -0400

    updated database migration

--- db/migrate/20140222220530_devise_create_users.rb
+++ db/migrate/20140222220530_devise_create_users.rb
@@ -1,6 +1,11 @@
 class DeviseCreateUsers < ActiveRecord::Migration
   def change
     create_table(:users) do |t|
+
+      # User information for Campus Anonymous
+      t.string  :name
+      t.integer :class_year
+
       ## Database authenticatable
       t.string :email,              :null => false, :default => ""
       t.string :encrypted_password, :null => false, :default => ""
@@ -30,13 +35,12 @@ class DeviseCreateUsers < ActiveRecord::Migration
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-
       t.timestamps
     end
 
     add_index :users, :email,                :unique => true
     add_index :users, :reset_password_token, :unique => true
-    # add_index :users, :confirmation_token,   :unique => true
+    add_index :users, :confirmation_token,   :unique => true
     # add_index :users, :unlock_token,         :unique => true
   end
 end
