13389b9d9ef93c74bcf08fd692d4e615d50ffce5
Akshay <aktwo@princeton.edu>
2014-03-30 13:29:43 -0400

    Added additional devise params to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,15 +9,5 @@ class User < ActiveRecord::Base
   belongs_to :school
 
   validates :email, presence: true, length: { maximum: 50 }
-  validates :school_name, presence: true
-  validate :email_must_belong_to_school
 
-  def email_must_belong_to_school
-    unless school.nil?
-      pattern = /\A[\w+\-.]+@#{school.name}.edu\z/i
-      if pattern.match(email).nil?
-        errors.add(:email, "email must be a #{school.name}.edu address")
-      end
-    end
-  end
 end
