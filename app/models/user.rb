c8e4ad5a2406c36c3b13a36d09be745b09b281c7
Akshay <aktwo@princeton.edu>
2014-04-06 20:53:46 -0400

    Added email validation for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,21 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable
 
-  devise :confirmable
   belongs_to :school
 
-  validates :email, presence: true, length: { maximum: 50 }
+  validate :email_must_belong_to_school_in_database
+
+  def email_must_belong_to_school_in_database
+    pattern = /\A[\w+\-.]+@(?<school_name>[\w+\-.]+).edu\z/i
+    if pattern.match(email).nil?
+      errors.add(:email, "email must be a valid Ivy League email address")
+    else
+      school_name = pattern.match(email)["school_name"]
+      unless School.all.map{|school| school.name}.include? school_name
+        errors.add(:email, "email must be a valid Ivy League email address")
+      end
+    end
+  end
+
 
 end
