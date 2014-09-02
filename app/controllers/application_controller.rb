fcb45945e59d2a836fb4f2700b705a6efe29a0e6
Dan Kang <dan@dskang.com>
2014-09-02 14:21:13 -0700

    add comment about allowing additional parameters for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  # permit additional parameters for devise
+  # https://github.com/plataformatec/devise#strong-parameters
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
   protected
