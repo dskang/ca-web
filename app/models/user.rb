813eaa0e15cc9a4a2185bfcb7c573d68ebdcbf8a
Akshay <aktwo@princeton.edu>
2014-09-28 15:29:22 -0400

    Added class year validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,18 +6,26 @@ class User < ActiveRecord::Base
 
   belongs_to :school
 
+  validate :class_year_must_be_reasonable
   validate :email_must_belong_to_school_in_database
   validates :email, presence: true, length: { maximum: 50 }, uniqueness: true
+
+  def class_year_must_be_reasonable
+    current_year = Time.now.year
+    unless class_year >= current_year and class_year < current_year + 4
+      errors.add(:class_year, :unreasonable_class_year)
+    end
+  end
 
   def email_must_belong_to_school_in_database
     pattern = /\A[\w+\-.]+@(?<school_name>[\w+\-.]+).edu\z/i
     p pattern.match(email)
     if pattern.match(email).nil?
-      errors.add(:email, "email must be a valid Ivy League email address")
+      errors.add(:email, :invalid_email)
     else
       school_name = pattern.match(email)["school_name"]
       unless School.find_by name: school_name
-        errors.add(:email, "email must be a valid Ivy League email address")
+        errors.add(:email, :invalid_email)
       end
     end
   end
