e5e8a61ba93e1249dc2d1a52a5b57bd705ec5190
Dan Kang <dan@dskang.com>
2014-12-30 18:46:43 -0800

    add user fixture

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,11 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   belongs_to :school
 
-  validates :name, :class_year, :email, :password, :school, presence: true
-  validates :email, length: { maximum: 50 }, uniqueness: true
+  validates :name, :class_year, :email, :school, presence: true
   validates :class_year, numericality: {
     only_integer: true,
     greater_than_or_equal_to: Time.now.year,
