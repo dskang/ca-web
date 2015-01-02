1960c9cdae2bbca0dfa3e30cc206d82229389f22
Akshay <aktwo@princeton.edu>
2015-01-01 19:33:29 -0500

    set user school based on email address

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,13 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   def new
-    super
+    super do |user|
+      pattern = /\A[\w+\-.]+@(?<school>.+).edu\z/i
+      match = pattern.match(user.email)
+      school = School.find_by(name: match[:school])
+      unless match.nil? or school.nil?
+        user.school = school
+      end
+    end
   end
 
   def create
