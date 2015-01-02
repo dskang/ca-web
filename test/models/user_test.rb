3eda710899b8e11f24c6ca826de9df1ea64ee2bb
Dan Kang <dan@dskang.com>
2015-01-02 00:23:19 -0800

    add tests for setting email from school

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -8,4 +8,25 @@ class UserTest < ActiveSupport::TestCase
   test "should save fixture without errors" do
     assert @user.save
   end
+
+  test "should set school from email" do
+    assert_equal 'princeton', @user.school.name
+    @user.email = 'hi@harvard.edu'
+    assert @user.save
+    assert_equal 'harvard', @user.school.name
+  end
+
+  test "should accept all ivy league schools" do
+    ivies = %w(princeton harvard yale brown upenn columbia dartmouth cornell)
+    ivies.each do |ivy|
+      @user.email = "hi@#{ivy}.edu"
+      assert @user.save
+    end
+  end
+
+  test "should reject non-ivy schools" do
+    @user.email = "hi@duke.edu"
+    assert_not @user.save
+  end
+
 end
