7b42e4aaec64b4fd00e56ce2c6815d973f6daba5
Dan Kang <dan@dskang.com>
2015-01-02 00:38:20 -0800

    add devise routes for user

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Ca::Application.routes.draw do
   root "static_pages#home"
   get "/chat" => "static_pages#chat"
 
+  devise_for :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
