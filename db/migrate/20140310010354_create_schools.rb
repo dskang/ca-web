2acb9f7c1b12d0b8562ae625eda78179fce749c7
Akshay <aktwo@princeton.edu>
2015-01-03 17:47:23 -0500

    update school migration to remove signups

--- db/migrate/20140310010354_create_schools.rb
+++ db/migrate/20140310010354_create_schools.rb
@@ -2,7 +2,6 @@ class CreateSchools < ActiveRecord::Migration
   def change
     create_table :schools do |t|
       t.string :name
-      t.integer :signups, default: 0
       t.timestamps
     end
 
