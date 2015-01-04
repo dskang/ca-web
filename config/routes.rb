866f5b2e9bbc0a5e96c6011338ca403cfaad1a2f
Akshay <aktwo@princeton.edu>
2015-01-03 19:58:50 -0500

    Modified default session/registration routes
    https://github.com/plataformatec/devise/wiki/How-To:-Change-the-default-sign_in-and-sign_out-routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,17 @@ Ca::Application.routes.draw do
   root "static_pages#home"
   get "/chat" => "static_pages#chat"
 
-  devise_for :users, controllers: {
+  devise_for :users, :skip => [:sessions, :registrations], controllers: {
     confirmations: "confirmations"
   }
+
+  devise_scope :user do
+    get '/login' => 'devise/sessions#new', :as => :new_user_session
+    post '/login' => 'devise/sessions#create', :as => :user_session
+    delete '/login' => 'devise/sessions#destroy', :as => :destroy_user_session
+    get '/signup' => 'devise/registrations#new', :as => :new_user_registration
+    post '/signup' => 'devise/registrations#create', :as => :user_registration
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
