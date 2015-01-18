9766cdca4cb2a261880c1f553b26604c693014de
Akshay <aktwo@princeton.edu>
2015-01-18 18:27:47 -0500

    Changed default sign-up message

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,7 +7,7 @@ class RegistrationsController < Devise::RegistrationsController
     yield resource if block_given?
     if resource_saved
       if resource.active_for_authentication?
-        set_flash_message :notice, :signed_up if is_flashing_format?
+        set_flash_message :notice, :signed_up_but_unconfirmed if is_flashing_format?
         sign_up(resource_name, resource)
         respond_with resource, location: after_sign_up_path_for(resource)
       else
