82436d54fb3dbd4e5b64bc5c36cea21d22057bde
Akshay <aktwo@princeton.edu>
2015-01-19 12:44:59 -0500

    Refactor session controller
    - Pass in block instead of modifying copied code

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,11 @@
 class SessionsController < Devise::SessionsController
 
   def create
-    self.resource = warden.authenticate!(auth_options)
-    if resource.confirmed?
-      set_flash_message(:notice, :signed_in) if is_flashing_format?
-    else
-      set_flash_message(:notice, :signed_in_but_unconfirmed) if is_flashing_format?
+    super do |user|
+      unless user.confirmed?
+        set_flash_message(:notice, :signed_in_but_unconfirmed) if is_flashing_format?
+      end
     end
-    sign_in(resource_name, resource)
-    yield resource if block_given?
-    respond_with resource, location: after_sign_in_path_for(resource)
   end
 
 end
