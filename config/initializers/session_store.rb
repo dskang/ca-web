3896c78f900b60a63bd07ecaeee92e114b4c8425
Dan Kang <dan@dskang.com>
2015-01-24 02:05:38 -0500

    share session with all subdomains

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Ca::Application.config.session_store :cookie_store, key: '_ca_session'
+Ca::Application.config.session_store :cookie_store, key: '_ca_session', domain: :all
