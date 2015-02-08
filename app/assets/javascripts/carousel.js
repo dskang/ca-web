c051547987fd7287dc7ca576f5e9346c681b438b
Akshay <aktwo@princeton.edu>
2015-02-08 16:39:34 -0500

    Cleaned up carousel.js and moved animate.css into /vendor/assets

--- app/assets/javascripts/carousel.js
+++ app/assets/javascripts/carousel.js
@@ -2,13 +2,15 @@
 
 $(document).ready(function(){
   var schools = $('.carousel');
-  schools.eq(0).addClass('carousel-entrance')
+  schools.eq(0).addClass('carousel-entrance');
   var counter = 0;
-  var numberOfSchools = schools.length;
 
   setInterval(function() {
-    schools.eq(counter).removeClass('carousel-entrance')
-    counter + 1 < numberOfSchools ? counter++ : counter = 0;
-    schools.eq(counter).addClass('carousel-entrance')
+    schools.eq(counter).removeClass('carousel-entrance');
+
+    counter++;
+    if (counter == schools.length) counter = 0;
+
+    schools.eq(counter).addClass('carousel-entrance');
   }, 2000);
 });
