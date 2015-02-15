0a83bf7a7da39d5bb6568f8b506abe17f9a1daaf
aktwo <aktwo@users.noreply.github.com>
2015-02-15 15:27:40 -0500

    Add comment for config.assets.digest = true

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,6 +32,7 @@ Ca::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-
+  
+  # Generate digests for assets URLs.
   config.assets.digest = true
 end
