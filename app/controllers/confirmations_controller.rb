7197581e4212c3ecf2962facdb64e63b5422060a
Akshay <akshay.kumar890@gmail.com>
2015-02-16 14:27:58 -0500

    Fix bug when users click confirmation email link more than once

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,5 +1,15 @@
 class ConfirmationsController < Devise::ConfirmationsController
 
+  def show
+    super do |resource|
+      unless resource.errors.empty?
+        set_flash_message :notice, :already_confirmed
+        redirect_to root_path
+        return
+      end
+    end
+  end
+
   protected
 
   def after_resending_confirmation_instructions_path_for(resource_name)
