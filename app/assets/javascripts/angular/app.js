0630e056ba76c750250221a586122d78c5da3d5d
Dan Kang <dan@dskang.com>
2015-02-17 01:44:29 -0500

    update google analytics snippet

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -10,9 +10,9 @@ var loadGoogleAnalytics = function(window) {
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,window.document,'script','//www.google-analytics.com/analytics.js','ga');
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-23357698-2', 'tigersanonymous.com');
+  ga('create', 'UA-23357698-3', 'auto');
   ga('send', 'pageview');
 };
 
