56939aa0987cc1e234932cd6236dde3d2e5ecaf1
Akshay <akshay.kumar890@gmail.com>
2015-02-16 20:35:06 -0500

    Remove hardcoded test URL and unnecessary comments in devise_mailer_preview.rb

--- lib/mailer_previews/devise_mailer_preview.rb
+++ lib/mailer_previews/devise_mailer_preview.rb
@@ -1,11 +1,10 @@
 class DeviseMailerPreview < ActionMailer::Preview
 
-  # Accessible from http://www.ca.local:3000/rails/mailers/devise_mailer/confirmation_instructions
+  # Previews accessible from http://localhost:3000/rails/mailers/
   def confirmation_instructions
     Devise::Mailer.confirmation_instructions(User.first, "test")
   end
 
-  # Accessible from http://www.ca.local:3000/rails/mailers/devise_mailer/reset_password_instructions
   def reset_password_instructions
     Devise::Mailer.reset_password_instructions(User.first, "test")
   end
