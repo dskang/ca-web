dbb2b00157100f06727900a2d4d88412834f2f10
Akshay <akshay.kumar890@gmail.com>
2015-03-01 00:17:29 -0500

    Update ActionMailer preview code to work when db is empty

--- lib/mailer_previews/devise_mailer_preview.rb
+++ lib/mailer_previews/devise_mailer_preview.rb
@@ -2,11 +2,17 @@ class DeviseMailerPreview < ActionMailer::Preview
 
   # Previews accessible from http://localhost:3000/rails/mailers/
   def confirmation_instructions
-    Devise::Mailer.confirmation_instructions(User.first, "test")
+    Devise::Mailer.confirmation_instructions(sample_user, "test")
   end
 
   def reset_password_instructions
-    Devise::Mailer.reset_password_instructions(User.first, "test")
+    Devise::Mailer.reset_password_instructions(sample_user, "test")
+  end
+
+  protected
+
+  def sample_user
+    User.new(email: "hi@princeton.edu", password: "password")
   end
 
 end
